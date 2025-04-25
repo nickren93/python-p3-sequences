b21685bcc4fafda25fcc36eac08446e2f8097712
Sitong <nickren93@gmail.com>
2025-04-24 20:59:37 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    #pass
+    fibonacciList = []
+    for num in range(length):
+        if num == 0:
+            fibonacciList.append(0) 
+        elif num == 1:
+            fibonacciList.append(1)
+        else:
+            new_element = fibonacciList[num-2] + fibonacciList[num-1]
+            fibonacciList.append(new_element)
+            
+    print(fibonacciList)
